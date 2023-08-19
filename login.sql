05d2766764755a133f50086b3546c2592cdedac5
Ali Turgut <ali_trgt@outlook.com>
2023-08-20 00:54:10 +0300

    Create login.sql

---
+++ login.sql
@@ -0,0 +1,11 @@
+CREATE TABLE login
+  (
+    id Serial,
+    name varchar(50),
+    surname varchar(50),
+    username varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    email varchar(50),
+    int balance
+  
+  );
